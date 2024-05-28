b150780113a18f43e442b61001f90f2f6630bbe3
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2024-05-28 16:20:38 +0800

    Tool Update

--- PSNR Tool/PSNR.py
+++ PSNR Tool/PSNR.py
@@ -5,5 +5,9 @@ from skimage.metrics import peak_signal_noise_ratio as psnr
 image1 = np.array(Image.open('Data/pic.jpg'))
 image2 = np.array(Image.open('Data/pic2.jpg'))
 
+image3 = np.array(Image.open('Data/astro.jpg'))
+image4 = np.array(Image.open('Data/astro2.jpg'))
+
 if __name__ == "__main__":
-    print(psnr(image1, image2))
+    print(round(psnr(image1, image2), 6))
+    print(round(psnr(image3, image4), 6))
